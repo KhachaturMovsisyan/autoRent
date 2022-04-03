3d9e11c397825b0439b08f27bdab3aa8dd1b36d1
xach <xach.movsisyan2020@mail.ru>
2022-04-03 20:24:23 +0400

    Add

--- src/main/java/com/example/autorent/AutoRentApplication.java
+++ src/main/java/com/example/autorent/AutoRentApplication.java
@@ -2,8 +2,11 @@ package com.example.autorent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {AutoRentApplication.class})
 public class AutoRentApplication {
 
     public static void main(String[] args) {
