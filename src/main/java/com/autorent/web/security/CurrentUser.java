18487e1ef81acce017d640921d84fa0b802527d3
Hayk <haykavetisyan882@gmail.com>
2022-05-08 00:30:13 +0400

    I18n

---
+++ src/main/java/com/autorent/web/security/CurrentUser.java
@@ -0,0 +1,19 @@
+package com.autorent.web.security;
+
+import com.autorent.web.entity.User;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class CurrentUser extends org.springframework.security.core.userdetails.User {
+
+    private User user;
+
+    public CurrentUser(User user) {
+        super(user.getEmail(),user.getPassword(),user.isActive(),true,true,true,AuthorityUtils.createAuthorityList(user.getUserType().name()));
+        this.user = user;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+}
